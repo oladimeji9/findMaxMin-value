d0e699c6f23a6d69d12b2b6bb8cbea6743f7f379
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-03-11 07:53:14 +0100

    This is my first commit

---
+++ app/findMinMax.js
@@ -0,0 +1,25 @@
+﻿exports.findMinMax = function(arr) {
+    var maxValue = arr[0];
+    var minValue = arr[0];
+
+    var result = new Array();
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+        }
+
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+        }
+    }
+
+    if (maxValue === minValue) {
+        result.push(minValue);
+    }
+    else {
+        result.push(minValue);
+        result.push(maxValue);
+    }
+    return result;
+}
